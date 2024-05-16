1ae588a51c3a169e71135d348813b235d6318a16
MOQRICHAyoub <ayoubmoqrich@gmail.com>
2024-05-16 14:22:25 +0200

    Remove Boilerplate Content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blogger-box-frontend';
+  // You can keep this class empty or add any logic you need here
 }
